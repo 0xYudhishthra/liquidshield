c0a9b4440617b43558e8c978a6cc6f03e101abb4
Yudhishthra Sugumaran <yudhishthra.m@gmail.com>
2026-03-18 22:55:07 +0000

    Revert "feat: integrate LiquidShield hook into Aqua0 frontend + backend"
    This reverts commit 6eb55f29d1c7dee0fbdaff9d292cb74c67fc4b31.

--- backend-hookathon/packages/api/test_read.ts
+++
@@ -1,7 +0,0 @@
-import { readAggregatedPositions, readPoolLiquidity } from "./src/contracts/v4-client.ts";
-async function run() {
-    const poolId = "0x4df014947c8607a870dccb9dd5f627a195c72c5faf5b27ba90dd9865a9d5b4f0";
-    console.log("Liquidity:", await readPoolLiquidity(696969, poolId as any));
-    console.log("Ranges:", await readAggregatedPositions(696969, poolId as any));
-}
-run().catch(console.error);
